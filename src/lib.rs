a8454dff4c114a10ee128292f7253cb6a98f0fc3
Katsu Uchiyama <katsu1112uchi@gmail.com>
2024-12-04 23:02:56 +0900

    Add function for PRC

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 mod echo_state_network;
 mod model;
+mod physical_reservoir;
 mod plot;
 mod serialize;
 mod utils;
 
 pub use echo_state_network::*;
 pub use model::*;
+pub use physical_reservoir::*;
 pub use plot::*;
 pub use serialize::*;
 pub use utils::*;
