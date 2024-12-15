b312b822b204d835b6b85ad4911723ee310d62f1
katsu <kaaatsu32329@gmail.com>
2024-12-16 00:00:48 +0900

    Update examples

---
+++ examples/narma.rs
@@ -0,0 +1,118 @@
+use echo_state_network::*;
+use rand::prelude::*;
+
+const TRAIN_STEP: usize = 10000;
+const TEST_STEP: usize = 500;
+const N_X: u64 = 1000;
+const BETA: f64 = 0.1;
+
+const NARMA_ALPHA: f64 = 0.4;
+const NARMA_BETA: f64 = 0.1;
+const NARMA_GAMMA: f64 = 0.1;
+const NARMA_DELTA: f64 = 0.1;
+
+const NARMA_STEP: usize = 10;
+
+const RANDOM_SEED: u64 = 42;
+const TEST_RANDOM_SEED: u64 = 92;
+
+fn main() {
+    let (train_input, train_expected_output) =
+        narma_n_data_gen(TRAIN_STEP, RANDOM_SEED, NARMA_STEP);
+    let (test_input, test_expected_output) =
+        narma_n_data_gen(TEST_STEP, TEST_RANDOM_SEED, NARMA_STEP);
+
+    let path = format!("{}/examples/graph", env!("CARGO_MANIFEST_DIR"));
+
+    let n_u = train_input.first().unwrap().len() as u64;
+    let n_y = train_expected_output.first().unwrap().len() as u64;
+
+    let mut model = EchoStateNetwork::new(
+        n_u,
+        n_y,
+        N_X,
+        0.1,
+        1.0,
+        0.9,
+        |x| x.tanh(),
+        None,
+        None,
+        1.0,
+        |x| x.clone_owned(),
+        |x| x.clone_owned(),
+        false,
+    );
+
+    let mut optimizer = Ridge::new(N_X, n_y, BETA);
+
+    model.train(&train_input, &train_expected_output, &mut optimizer);
+
+    let estimated_output = model.estimate(&test_input);
+
+    let (l2_error, l1_error) = get_error_rate(
+        estimated_output.clone(),
+        test_expected_output.clone(),
+        NARMA_STEP,
+    );
+    println!("Mean Squared Error: {}", l2_error);
+    println!("Mean Absolute Error: {}", l1_error);
+
+    let y_estimated = estimated_output.iter().map(|x| x[0]).collect::<Vec<f64>>();
+    let y_expected = test_expected_output
+        .clone()
+        .into_iter()
+        .flatten()
+        .collect::<Vec<f64>>();
+
+    plotter::plot(
+        "NARMA",
+        (0..TEST_STEP).map(|v| v as f64).collect::<Vec<f64>>(),
+        vec![y_expected, y_estimated],
+        vec!["Expected".to_string(), "Estimated".to_string()],
+        Some(&path),
+    )
+    .unwrap();
+}
+
+fn narma_n_data_gen(step: usize, seed: u64, n: usize) -> (Vec<Vec<f64>>, Vec<Vec<f64>>) {
+    let mut rng = StdRng::seed_from_u64(seed);
+
+    let input_vec = (0..step)
+        .map(|_| vec![rng.gen_range(0.0..1.0)])
+        .collect::<Vec<Vec<f64>>>();
+
+    let mut output_vec = vec![vec![0.0]; step];
+
+    for i in n..step {
+        let mut state_sum = 0.0;
+        for j in 0..n {
+            state_sum += input_vec[i - j][0];
+        }
+        output_vec[i][0] = NARMA_ALPHA * output_vec[i - 1][0]
+            + NARMA_BETA * output_vec[i - 1][0] * state_sum
+            + NARMA_GAMMA * input_vec[i - n + 1][0] * input_vec[i - 1][0]
+            + NARMA_DELTA;
+    }
+
+    (input_vec, output_vec)
+}
+
+fn get_error_rate(
+    estimated_output: Vec<Vec<f64>>,
+    expected_output: Vec<Vec<f64>>,
+    ignore_bits: usize,
+) -> (f64, f64) {
+    let estimated_output = estimated_output.iter().map(|x| x[0]).collect::<Vec<f64>>();
+    let expected_output = expected_output.into_iter().flatten().collect::<Vec<f64>>();
+
+    let mse = mean_squared_error(
+        &expected_output[ignore_bits..],
+        &estimated_output[ignore_bits..],
+    );
+    let mae = mean_absolute_error(
+        &expected_output[ignore_bits..],
+        &estimated_output[ignore_bits..],
+    );
+
+    (mse, mae)
+}
--- examples/xor.rs
+++ examples/xor.rs
@@ -10,8 +10,8 @@ const RANDOM_SEED: u64 = 41;
 const TEST_RANDOM_SEED: u64 = 91;
 
 fn main() {
-    let (train_input, train_expected_output) = data_gen(TRAIN_STEP, RANDOM_SEED);
-    let (test_input, test_expected_output) = data_gen(TEST_STEP, TEST_RANDOM_SEED);
+    let (train_input, train_expected_output) = xor_data_gen(TRAIN_STEP, RANDOM_SEED);
+    let (test_input, test_expected_output) = xor_data_gen(TEST_STEP, TEST_RANDOM_SEED);
 
     let path = format!("{}/examples/graph", env!("CARGO_MANIFEST_DIR"));
 
@@ -41,9 +41,9 @@ fn main() {
     let estimated_output = model.estimate(&test_input);
 
     let (bits_l2_error, bits_l1_error) =
-        get_bits_error_rate(estimated_output.clone(), test_expected_output.clone());
+        get_bits_error_rate(estimated_output.clone(), test_expected_output.clone(), 2);
     let (l2_error, l1_error) =
-        get_error_rate(estimated_output.clone(), test_expected_output.clone());
+        get_error_rate(estimated_output.clone(), test_expected_output.clone(), 2);
     println!("Bits Mean Squared Error: {}", bits_l2_error);
     println!("Bits Mean Absolute Error: {}", bits_l1_error);
     println!("Mean Squared Error: {}", l2_error);
@@ -77,27 +77,27 @@ fn main() {
     );
 }
 
-fn data_gen(step: usize, seed: u64) -> (Vec<Vec<f64>>, Vec<Vec<f64>>) {
+fn xor_data_gen(step: usize, seed: u64) -> (Vec<Vec<f64>>, Vec<Vec<f64>>) {
     let tau = 2;
 
     let mut rng = rand::rngs::StdRng::seed_from_u64(seed);
 
-    let train_input_vec = (0..step)
+    let input_vec = (0..step)
         .map(|_| vec![rng.gen_range(0..2) as f64])
         .collect::<Vec<Vec<f64>>>();
 
-    let mut expected_output_vec = vec![vec![0.0]; step];
+    let mut output_vec = vec![vec![0.0]; step];
     for n in tau..step {
-        expected_output_vec[n][0] =
-            ((train_input_vec[n - 1][0] as u32) ^ (train_input_vec[n - 2][0] as u32)) as f64;
+        output_vec[n][0] = ((input_vec[n - 1][0] as u32) ^ (input_vec[n - 2][0] as u32)) as f64;
     }
 
-    (train_input_vec, expected_output_vec)
+    (input_vec, output_vec)
 }
 
 fn get_bits_error_rate(
     estimated_output: Vec<Vec<f64>>,
     expected_output: Vec<Vec<f64>>,
+    ignore_bits: usize,
 ) -> (f64, f64) {
     let mut y_tested_binary = vec![0.0; estimated_output.len()];
 
@@ -111,18 +111,34 @@ fn get_bits_error_rate(
 
     let expected_output = expected_output.into_iter().flatten().collect::<Vec<f64>>();
 
-    let mse = mean_squared_error(&expected_output, &y_tested_binary);
-    let mae = mean_absolute_error(&expected_output, &y_tested_binary);
+    let mse = mean_squared_error(
+        &expected_output[ignore_bits..],
+        &y_tested_binary[ignore_bits..],
+    );
+    let mae = mean_absolute_error(
+        &expected_output[ignore_bits..],
+        &y_tested_binary[ignore_bits..],
+    );
 
     (mse, mae)
 }
 
-fn get_error_rate(estimated_output: Vec<Vec<f64>>, expected_output: Vec<Vec<f64>>) -> (f64, f64) {
+fn get_error_rate(
+    estimated_output: Vec<Vec<f64>>,
+    expected_output: Vec<Vec<f64>>,
+    ignore_bits: usize,
+) -> (f64, f64) {
     let estimated_output = estimated_output.iter().map(|x| x[0]).collect::<Vec<f64>>();
     let expected_output = expected_output.into_iter().flatten().collect::<Vec<f64>>();
 
-    let mse = mean_squared_error(&expected_output, &estimated_output);
-    let mae = mean_absolute_error(&expected_output, &estimated_output);
+    let mse = mean_squared_error(
+        &expected_output[ignore_bits..],
+        &estimated_output[ignore_bits..],
+    );
+    let mae = mean_absolute_error(
+        &expected_output[ignore_bits..],
+        &estimated_output[ignore_bits..],
+    );
 
     (mse, mae)
 }
