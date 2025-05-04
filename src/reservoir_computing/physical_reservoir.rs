5f4ce848ce77f7320f7b0413c009422b08af6aee
Katsu Uchiyama <44460592+kaaatsu32329@users.noreply.github.com>
2025-05-04 17:27:47 +0900

    Add function for PRC (#6)
    * Add function for PRC
    
    * Update PRC
    
    * Add test
    
    * Fix

--- src/reservoir_computing/physical_reservoir.rs
+++ src/reservoir_computing/physical_reservoir.rs
@@ -17,6 +17,19 @@ impl PhysicalReservoir {
         }
     }
 
+    /// Create a new PhysicalReservoir with parameters.
+    /// n_y: The number of output nodes.
+    /// n_x: The number of input(sensor) nodes.
+    /// rls_param: Parameters for RLS. (forgetting_factor, regularization_parameter)
+    /// ridge_param: Parameter for Ridge regression. Regularization parameter.
+    pub fn new_with_param(n_y: u64, n_x: u64, rls_param: (f64, f64), ridge_param: f64) -> Self {
+        PhysicalReservoir {
+            output: Output::new(n_y, n_x),
+            rls: Some(RLS::new(n_x, n_y, rls_param.0, rls_param.1)),
+            ridge: Some(Ridge::new(n_x, n_y, ridge_param)),
+        }
+    }
+
     pub fn readout_weight(&self) -> &na::DMatrix<f64> {
         self.output.output_weight()
     }
@@ -26,32 +39,31 @@ impl ReservoirComputing for PhysicalReservoir {
     /// Online training method.
     /// teaching_input: Input data for training. In this case, it is a sensor data from the physical reservoir.
     fn train(&mut self, teaching_input: &[f64], teaching_output: &[f64]) {
-        match &mut self.rls {
-            Some(rls) => {
-                let x = na::DVector::from_vec(teaching_input.to_vec());
-                let d = na::DVector::from_vec(teaching_output.to_vec());
-                rls.set_data(&x, &d);
-                let weight = rls.fit();
-                self.output.set_weight(weight);
-            }
-            None => panic!("RLS is not initialized"),
+        if self.rls.is_none() {
+            panic!("RLS is not initialized");
         }
+        let x = na::DVector::from_vec(teaching_input.to_vec());
+        let d = na::DVector::from_vec(teaching_output.to_vec());
+        let rls = self.rls.as_mut().unwrap();
+        rls.set_data(&x, &d);
+        let weight = rls.fit();
+        self.output.set_weight(weight);
     }
 
     fn offline_train(&mut self, teaching_input: &[Vec<f64>], teaching_output: &[Vec<f64>]) {
-        match &mut self.ridge {
-            Some(ridge) => {
-                for (input, output) in teaching_input.iter().zip(teaching_output.iter()) {
-                    let x = na::DVector::from_vec(input.clone());
-                    let d = na::DVector::from_vec(output.clone());
-                    ridge.set_data(&x, &d);
-                }
-
-                let weight = ridge.fit();
-                self.output.set_weight(weight);
-            }
-            None => panic!("Ridge is not initialized"),
+        if self.ridge.is_none() {
+            panic!("Ridge is not initialized");
         }
+
+        let ridge = self.ridge.as_mut().unwrap();
+        for (input, output) in teaching_input.iter().zip(teaching_output.iter()) {
+            let x = na::DVector::from_vec(input.clone());
+            let d = na::DVector::from_vec(output.clone());
+            ridge.set_data(&x, &d);
+        }
+
+        let weight = ridge.fit();
+        self.output.set_weight(weight);
     }
 
     /// Estimate method.
@@ -60,5 +72,25 @@ impl ReservoirComputing for PhysicalReservoir {
         let x = na::DVector::from_vec(input.to_vec());
         let output = self.output.call(&x);
         output.data.as_slice().to_vec()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_approx_eq::assert_approx_eq;
+
+    #[test]
+    fn test_physical_reservoir() {
+        let mut reservoir = PhysicalReservoir::new_with_param(1, 2, (0.0, 0.0), 0.0);
+        let teaching_input = vec![vec![1.0, 2.0], vec![3.0, 4.0]];
+        let teaching_output = vec![vec![1.5], vec![3.5]];
+
+        reservoir.offline_train(&teaching_input, &teaching_output);
+
+        let input = vec![5.0, 6.0];
+        let output = reservoir.estimate(&input);
+        let expected_output = 5.5;
+        assert_approx_eq!(output[0], expected_output);
     }
 }
